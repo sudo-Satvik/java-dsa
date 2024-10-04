3de58489dc2e6512aa3d13caddbadab8b969b061
Satvik <sharmasatvik43@gmail.com>
2024-10-04 19:48:36 +0530

    TwoSum problem done

--- src/data_structure/array/SecondThirdSmallest.java
+++ src/data_structure/array/SecondThirdSmallest.java
@@ -4,22 +4,21 @@ import java.util.Arrays;
 
 public class SecondThirdSmallest {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 3, 2, 5, 6, 11, 34};
-        System.out.println(Arrays.toString(kthSmallestLargest(arr, 2)));
+        int[] arr = {1, 2, 3, 4, 3, 2, 5, 6, 34};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(secondSmallestLargest(arr)));
     }
-    private static int[] kthSmallestLargest(int[] arr, int k){
+    private static int[] secondSmallestLargest(int[] arr){
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
-        for (int i = 1; i <= k; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                if (i == k && arr[j] < min) {
-                    min = arr[j];
-                }
-                if (i == k && arr[j] > max) {
-                    max = arr[j];
-                }
+        for (int i = 0; i < arr.length; i++) {
+            if (min > arr[i]){
+                min = arr[i + 1];
+            }
+            if (max < arr[i] && i != 0){
+                max = arr[i - 1];
             }
         }
-        return new int[] {min, max};
+        return new int[]{min, max};
     }
 }
