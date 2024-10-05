ec48acb4a7544b1d6c06bebc492f83b29683d3f0
Satvik <sharmasatvik43@gmail.com>
2024-10-05 22:10:18 +0530

    Array Basic Pt.2 done

---
+++ src/data_structure/array/Rotating1.java
@@ -0,0 +1,24 @@
+package data_structure.array;
+
+import java.util.Arrays;
+
+//Rotating array by kth element with O(n) space complexity
+public class Rotating1 {
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 6, 7, 3, 2, 5};
+        rotate(arr, 3);
+    }
+    public static void rotate(int[] arr, int k){
+        int n = arr.length, j = 0;
+        int[] temp = new int[n];
+//        First half
+        for (int i = n - k; i < n; i++) {
+            temp[j++] = arr[i];
+        }
+//        Second half
+        for (int i = 0; i < n - k ; i++) {
+            temp[j++] = arr[i];
+        }
+        System.out.println(Arrays.toString(temp));
+    }
+}
