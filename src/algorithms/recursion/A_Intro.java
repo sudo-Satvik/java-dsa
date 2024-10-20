a08662c089d327cf73e8e8012f49ed33d87c70c0
Satvik <sharmasatvik43@gmail.com>
2024-10-20 11:20:33 +0530

    Recursion Revised

--- src/algorithms/recursion/A_Intro.java
+++ src/algorithms/recursion/A_Intro.java
@@ -3,17 +3,28 @@ package algorithms.recursion;
 public class A_Intro {
     public static void main(String[] args) {
         int n = 12;
-        printNum(n);
+        printNumIncreasing(n);
+        System.out.println("");
+        printNumDecreasing(n);
     }
-    public static void printNum(int n){
-        // Base case
+    public static void printNumIncreasing(int n){
+        // Base case or Halting Condition which help to prevent stack overflow!
         if (n == 1){
             System.out.print(1+" ");
             return;
         }
-        // Assumption
-        printNum(n - 1);
+        // Assumption aka smaller or sub problem   
+        printNumIncreasing(n - 1);
         // Self Work
         System.out.print(n+" ");
+    }
+    public static void printNumDecreasing(int n){
+//        Halting condition
+        if (n == 1){
+            System.out.print(1 + " ");
+            return;
+        }
+        System.out.print(n+" ");
+        printNumDecreasing(n-1);
     }
 }
