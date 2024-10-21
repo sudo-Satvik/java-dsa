955f64a73eadbb9ee550cd5dc93f4ef134ac191d
Satvik <sharmasatvik43@gmail.com>
2024-10-21 16:02:05 +0530

    count the digits

--- src/algorithms/recursion/E_SumOfDigits.java
+++ src/algorithms/recursion/E_SumOfDigits.java
@@ -4,6 +4,7 @@ public class E_SumOfDigits {
     public static void main(String[] args) {
         int n = 1234;
         System.out.println(addingDigits(n));
+        System.out.println(countDigits(n));
     }
 
     // Sub problem in this:
@@ -11,10 +12,15 @@ public class E_SumOfDigits {
 
     public static int addingDigits(int n){
         // Halting Condition
-        if (n == 1) {
+        if (n <= 9 && n >= 0) {
             return n;
         }
         // Self Work & Recursive Work
         return addingDigits(n / 10) + n % 10;
+    }
+    public static int countDigits(int n){
+        // Base Case
+        if (n <= 9 && n >= 1) return 1;
+        return countDigits(n / 10) + 1;
     }
 }
