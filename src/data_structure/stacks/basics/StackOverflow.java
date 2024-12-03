edf59ccf9749d67ac61014ab72fc6e4e67e8e6c1
Satvik <sharmasatvik43@gmail.com>
2024-12-03 16:37:40 +0530

    stack overflow done

---
+++ src/data_structure/stacks/basics/StackOverflow.java
@@ -0,0 +1,8 @@
+package data_structure.stacks.basics;
+/*
+stack overflow refers to a condition where the stack data structure exceeds its capacity, leading to an error. It can
+happen in programming environments where memory for the stack is limited.
+* */
+public class StackOverflow {
+
+}
