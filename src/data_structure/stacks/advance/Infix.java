e67179fab0a5a59baccd17e3f7990c605ac8108e
Satvik <sharmasatvik43@gmail.com>
2024-12-11 00:14:33 +0530

    infix done and understood

--- src/data_structure/stacks/advance/Infix.java
+++ src/data_structure/stacks/advance/Infix.java
@@ -1,52 +1,49 @@
 package data_structure.stacks.advance;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Infix {
     public static void main(String[] args) {
-        String str = "9-5+3*4/6";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the algebraic equation!");
+        String str = sc.nextLine();
         Stack<Integer> val = new Stack<>();
         Stack<Character> op = new Stack<>();
-        for(int i = 0 ; i < str.length() ; i++) {
+        for (int i = 0 ; i < str.length() ; i++) {
             char ch = str.charAt(i);
-            int ascii = (int) ch;
-            // Check for the number
-            if (ascii>=48 && ascii<=57) val.push(ascii-48);
-            // For operators
-            else if (op.isEmpty() || ch == '(') op.push(ch);
+            if ((int)ch >= 48 && (int)ch <= 57) val.push((int)ch - 48);
+            else if (op.isEmpty()) op.push(ch);
             else{
-                if (ch == '+' || ch == '-'){
-                    // Work
-                    int v2 = val.pop();
-                    int v1 = val.pop();
-                    if (op.peek() == '+') val.push(v1 + v2);
-                    if (op.peek() == '-') val.push(v1 - v2);
-                    if (op.peek() == '*') val.push(v1 * v2);
-                    if (op.peek() == '/') val.push(v1 / v2);
+                if (ch =='+' || ch == '-') {
+                    int val2 = val.pop();
+                    int val1 = val.pop();
+                    // Performing the work
+                    if (op.peek() == '+') val.push(val1 + val2);
+                    if (op.peek() == '-') val.push(val1 - val2);
+                    if (op.peek() == '/') val.push(val1 / val2);
+                    if (op.peek() == '*') val.push(val1 * val2);
                     op.pop();
                     op.push(ch);
-                }
-                if (ch == '*' || ch == '/') {
-                    if (op.peek() == '*' || op.peek() == '/') {
-                        int v2 = val.pop();
-                        int v1 = val.pop();
-                        if(op.peek() == '*') val.push(v2*v1);
-                        if(op.peek() == '/') val.push(v2/v1);
+                } if (ch =='*' || ch == '/'){
+                    if (op.peek() == '*' || op.peek() == '/'){
+                        int val2 = val.pop();
+                        int val1 = val.pop();
+                        if (op.peek() == '/') val.push(val1 / val2);
+                        if (op.peek() == '*') val.push(val1 * val2);
                         op.pop();
                         op.push(ch);
-                    } else op.push(ch);
+                    }else op.push(ch);
                 }
             }
         }
-
-        // Now val stack need to be 1 size
-        while(val.size() > 1) {
-            int v2 = val.pop();
-            int v1 = val.pop();
-            if (op.peek() == '-') val.push(v1 - v2);
-            if (op.peek() == '+') val.push(v1 + v2);
-            if (op.peek() == '*') val.push(v1 * v2);
-            if (op.peek() == '/') val.push(v1 / v2);
+        while (val.size() > 1) {
+            int val2 = val.pop();
+            int val1 = val.pop();
+            if (op.peek() == '+') val.push(val1 + val2);
+            if (op.peek() == '-') val.push(val1 - val2);
+            if (op.peek() == '/') val.push(val1 / val2);
+            if (op.peek() == '*') val.push(val1 * val2);
             op.pop();
         }
         System.out.println(val.peek());
