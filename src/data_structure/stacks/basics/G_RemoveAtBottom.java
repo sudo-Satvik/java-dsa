f6bf745127d9101abf2f8f9260f931e0931498a6
Satvik <sharmasatvik43@gmail.com>
2024-12-23 21:00:58 +0530

    remove from the bottom of the stack logic is done

---
+++ src/data_structure/stacks/basics/G_RemoveAtBottom.java
@@ -0,0 +1,32 @@
+package data_structure.stacks.basics;
+import java.util.Stack;
+
+/*Remove the element from bottom*/
+
+public class G_RemoveAtBottom {
+    public static int removeFromBottom(Stack<Integer> st) {
+        Stack<Integer> temp = new Stack<>();
+        // st -> temp
+        while (!st.isEmpty()){
+            temp.push(st.pop());
+        }
+        int removed = temp.pop();
+        // temp -> st
+        while (!temp.isEmpty()) {
+            st.push(temp.pop());
+        }
+        return removed;
+    }
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(5);
+        st.push(6);
+        st.push(7);
+        st.push(8);
+        st.push(9);
+
+        System.out.println("Original Stack: " + st);
+        removeFromBottom(st);
+        System.out.println("Final Stack: " + st);
+    }
+}
