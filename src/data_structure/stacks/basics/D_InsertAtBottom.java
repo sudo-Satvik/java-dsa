9969b01c6c1b0e891e7926ffc517dcb29b5bd301
Satvik <sharmasatvik43@gmail.com>
2024-12-23 17:24:17 +0530

    done insert at bottom

---
+++ src/data_structure/stacks/basics/D_InsertAtBottom.java
@@ -0,0 +1,39 @@
+package data_structure.stacks.basics;
+
+// Inserting an element at the bottom of the stack
+
+import java.util.Stack;
+
+public class D_InsertAtBottom {
+    public static void insertAtBottom(Stack<Integer> st, int val){
+        Stack<Integer> temp = new Stack<>();
+
+        // st -> temp
+        while (!st.isEmpty()) {
+            temp.push(st.pop());
+        }
+        temp.push(val);
+
+        // temp -> st
+        while (!temp.isEmpty()) {
+            st.push(temp.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> st = new Stack<>();
+        st.push(12);
+        st.push(85);
+        st.push(19);
+        st.push(55);
+        st.push(71);
+
+        System.out.println("Original Stack: "+st);
+
+        insertAtBottom(st, 100);
+        System.out.println("After adding 100 at bottom: "+st);
+
+        insertAtBottom(st, 2000);
+        System.out.println("After adding 2000 at bottom: "+st);
+    }
+}
