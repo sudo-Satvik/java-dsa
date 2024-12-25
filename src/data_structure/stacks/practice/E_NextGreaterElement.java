2c521e7a320149ba59ade3ab00560df3c19c845e
Satvik <sharmasatvik43@gmail.com>
2024-12-25 20:16:20 +0530

    next greater element by bruteforce method is added

--- src/data_structure/stacks/practice/E_NextGreaterElement.java
+++ src/data_structure/stacks/practice/E_NextGreaterElement.java
@@ -25,8 +25,23 @@ public class E_NextGreaterElement {
         return res;
     }
 
+    public static int[] optimisedNextGreaterElement(int[] arr) {
+        int n = arr.length;
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
+        res[n - 1] = -1;
+        st.push(arr[n-1]);
+        for (int i = n - 2; i >= 0 ; i--) {
+            while (!st.isEmpty() && st.peek() < arr[i]) st.pop();
+            if (st.isEmpty()) res[i] = -1;
+            else res[i] = st.peek();
+            st.push(arr[i]);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 3, 2, 1, 8, 6, 3, 4};
-        System.out.println("Next Greater by bruteforce: " + Arrays.toString(nextGreaterBruteforce(arr)));
+        System.out.println("Next Greater by bruteforce: " + Arrays.toString(optimisedNextGreaterElement(arr)));
     }
 }
