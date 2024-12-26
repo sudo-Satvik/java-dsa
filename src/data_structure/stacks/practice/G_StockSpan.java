6e5ca09e91ca4c207e50b5691b6a9c8f4e4c761a
Satvik <sharmasatvik43@gmail.com>
2024-12-26 16:47:43 +0530

    stockspan problem done

--- src/data_structure/stacks/practice/G_StockSpan.java
+++ src/data_structure/stacks/practice/G_StockSpan.java
@@ -9,18 +9,34 @@ import java.util.*;
  * */
 
 public class G_StockSpan {
-//    public static ArrayList<Integer> calculateSpan(int[] arr) {
-//
-//    }
-//    public static void main(String[] args) {
-//        int[] testCase1 = {100, 80, 60, 70, 60, 75, 85};        // [1, 1, 1, 2, 1, 4, 6]
-//        int[] testCase2 = {10, 4, 5, 90, 120, 80};              // [1, 1, 2, 4, 5, 1]
-//        int[] testCase3 = {11, 4, 5, 90, 120, 80};              // [1, 1, 2, 4, 5, 1]
-//
-//        System.out.println("Answer1: " + calculateSpan(testCase1));
-//        System.out.println("====================================");
-//        System.out.println("Answer2: " + calculateSpan(testCase2));
-//        System.out.println("====================================");
-//        System.out.println("Answer3: " + calculateSpan(testCase3));
-//    }
+        // Function to calculate the span of stock's price for all n days.
+    public static ArrayList<Integer> calculateSpan(int[] arr) {
+        ArrayList<Integer> span = new ArrayList<>();
+        Stack<Integer> st = new Stack<>();
+
+        for (int i = 0 ; i < arr.length ; i++) {
+            while (!st.isEmpty() && arr[st.peek()] <= arr[i]) {
+                st.pop();
+            }
+
+            if (st.isEmpty()) span.add(i + 1);
+            else span.add(i - st.peek());
+
+            st.push(i);
+        }
+        return span;
+    }
+    public static void main(String[] args) {
+        int[] testCase1 = {100, 80, 60, 70, 60, 75, 85};        // [1, 1, 1, 2, 1, 4, 6]
+        int[] testCase2 = {10, 4, 5, 90, 120, 80};              // [1, 1, 2, 4, 5, 1]
+        int[] testCase3 = {11, 4, 5, 90, 120, 80};              // [1, 1, 2, 4, 5, 1]
+        System.out.println("Stocks: " + Arrays.toString(testCase1));
+        System.out.println("Answer1: " + calculateSpan(testCase1));
+        System.out.println("====================================");
+        System.out.println("Stocks: " + Arrays.toString(testCase2));
+        System.out.println("Answer2: " + calculateSpan(testCase2));
+        System.out.println("====================================");
+        System.out.println("Stocks: " + Arrays.toString(testCase3));
+        System.out.println("Answer3: " + calculateSpan(testCase3));
+    }
 }
