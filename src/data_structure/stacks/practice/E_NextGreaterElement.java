e2c34ac5b6910879d17865563e64b9702e9cb54c
Satvik <sharmasatvik43@gmail.com>
2024-12-26 15:36:53 +0530

    next greater element by optimised method is added

--- src/data_structure/stacks/practice/E_NextGreaterElement.java
+++ src/data_structure/stacks/practice/E_NextGreaterElement.java
@@ -28,13 +28,18 @@ public class E_NextGreaterElement {
     public static int[] optimisedNextGreaterElement(int[] arr) {
         int n = arr.length;
         int[] res = new int[n];
-        Stack<Integer> st = new Stack<>();
         res[n - 1] = -1;
-        st.push(arr[n-1]);
-        for (int i = n - 2; i >= 0 ; i--) {
-            while (!st.isEmpty() && st.peek() < arr[i]) st.pop();
-            if (st.isEmpty()) res[i] = -1;
-            else res[i] = st.peek();
+        Stack<Integer> st = new Stack<>();
+        st.push(arr[n - 1]);
+        for (int i = n - 2; i >= 0; i--) {
+            while (st.size() > 0 && st.peek() < arr[i]) {
+                st.pop();
+            }
+            if (st.size() == 0) {
+                res[i] = -1;
+            } else {
+                res[i] = st.peek();
+            }
             st.push(arr[i]);
         }
         return res;
@@ -42,6 +47,7 @@ public class E_NextGreaterElement {
 
     public static void main(String[] args) {
         int[] arr = {1, 3, 2, 1, 8, 6, 3, 4};
-        System.out.println("Next Greater by bruteforce: " + Arrays.toString(optimisedNextGreaterElement(arr)));
+        System.out.println("Next Greater by bruteforce -> " + Arrays.toString(nextGreaterBruteforce(arr)));
+        System.out.println("Next Greater by optimised  -> " + Arrays.toString(optimisedNextGreaterElement(arr)));
     }
 }
