d5a0240f78dc877a86f03dd8da224d7eaf55a3c2
Satvik <sharmasatvik43@gmail.com>
2024-12-26 16:13:50 +0530

    previous greater element is implemented by my own

---
+++ src/data_structure/stacks/practice/G_StockSpan.java
@@ -0,0 +1,26 @@
+package data_structure.stacks.practice;
+import java.util.*;
+
+/**
+ * @Question: The stock span problem is a financial problem where we have a series of daily price quotes for a stock and we need to calculate the span of stock price for all days. The span arr[i] of the stocks price on a given day i is defined as the maximum number of consecutive days just before the given day, for which the price of the stock on the given day is less than or equal to its price on the current day.
+ *
+ * @Example: arr[] = [100, 80, 60, 70, 60, 75, 85] ==> [1, 1, 1, 2, 1, 4, 6]
+ * @Example2: arr[] = [10, 4, 5, 90, 120, 80] ==> [1, 1, 2, 4, 5, 1]
+ * */
+
+public class G_StockSpan {
+//    public static ArrayList<Integer> calculateSpan(int[] arr) {
+//
+//    }
+//    public static void main(String[] args) {
+//        int[] testCase1 = {100, 80, 60, 70, 60, 75, 85};        // [1, 1, 1, 2, 1, 4, 6]
+//        int[] testCase2 = {10, 4, 5, 90, 120, 80};              // [1, 1, 2, 4, 5, 1]
+//        int[] testCase3 = {11, 4, 5, 90, 120, 80};              // [1, 1, 2, 4, 5, 1]
+//
+//        System.out.println("Answer1: " + calculateSpan(testCase1));
+//        System.out.println("====================================");
+//        System.out.println("Answer2: " + calculateSpan(testCase2));
+//        System.out.println("====================================");
+//        System.out.println("Answer3: " + calculateSpan(testCase3));
+//    }
+}
