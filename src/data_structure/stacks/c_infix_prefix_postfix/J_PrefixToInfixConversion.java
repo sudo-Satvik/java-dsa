2c11e800c53483767b98c77ce8bb3a6880f2294d
Satvik <sharmasatvik43@gmail.com>
2024-12-28 13:37:58 +0530

    Postfix to Infix Conversion done by own

---
+++ src/data_structure/stacks/c_infix_prefix_postfix/J_PrefixToInfixConversion.java
@@ -0,0 +1,7 @@
+package data_structure.stacks.c_infix_prefix_postfix;
+
+public class J_PrefixToInfixConversion {
+    public static void main(String[] args) {
+        String prefix = "-9/*+5346";
+    }
+}
