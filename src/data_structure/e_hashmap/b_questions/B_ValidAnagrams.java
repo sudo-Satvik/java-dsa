9459b506398c9838a3ae8d01aa9b35bacf75842f
Satvik <sharmasatvik43@gmail.com>
2025-01-14 21:57:14 +0530

    Initiated valid anagram

---
+++ src/data_structure/e_hashmap/b_questions/B_ValidAnagrams.java
@@ -0,0 +1,17 @@
+package data_structure.e_hashmap.b_questions;
+import java.util.*;
+/** @Quest: Check whether two strings are valid anagrams or not. Return true if they are else false.
+ * @Anagram: An anagram of a string is another string that contains the same characters, only the order of character can be different.
+ * @Example1: "abcd" -> "bcad"
+ * @Example2: "silent" -> "listen"
+ */
+
+public class B_ValidAnagrams {
+    public static void main(String[] args) {
+        /*To valid anagram:-
+        1. The Length of both strings must be same
+        2. The Frequency of each character must be the same
+        3. No character must be extra different
+        */
+    }
+}
