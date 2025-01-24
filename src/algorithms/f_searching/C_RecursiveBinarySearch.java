9800261edca5960948b6959aa3e0318b8ab69f75
Satvik <sharmasatvik43@gmail.com>
2025-01-24 12:07:56 +0530

    Recursive Binary Search Coding implementation is done

--- src/algorithms/f_searching/C_RecursiveBinarySearch.java
+++ src/algorithms/f_searching/C_RecursiveBinarySearch.java
@@ -1,7 +1,18 @@
 package algorithms.f_searching;
 
 public class C_RecursiveBinarySearch {
+    public static int recursiveBinarySearch(int[] arr, int start, int end, int target) {
+        if (start > end) return -1;
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == target)
+            return mid;
+        else if (arr[mid] < target)
+            return recursiveBinarySearch(arr, mid + 1, end, target);
+        else
+            return recursiveBinarySearch(arr, start, mid - 1, target);
+    }
     public static void main(String[] args) {
-
+        int[] arr = {12, 21, 23, 34, 45, 55, 67, 93};
+        System.out.println(recursiveBinarySearch(arr, 0, arr.length - 1, 55));
     }
 }
