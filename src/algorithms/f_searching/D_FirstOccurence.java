ec58604928e3e6f0bc2edb49d49d1ddbfd4cc1d5
Satvik <sharmasatvik43@gmail.com>
2025-01-24 13:11:39 +0530

    First Occurence Problem is done along with sqrt problem

---
+++ src/algorithms/f_searching/D_FirstOccurence.java
@@ -0,0 +1,26 @@
+package algorithms.f_searching;
+
+public class D_FirstOccurence {
+    public static int firstOccurence(int[] arr, int target) {
+        int n = arr.length - 1;
+        int start = 0, end = n;
+        int ans = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                ans = mid;
+                end = mid - 1;
+            }
+            else if (arr[mid] > target)
+                end = mid - 1;
+            else
+                start = mid + 1;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 5, 5, 6, 6, 8, 9, 9, 9};
+        int[] arr2 = {1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4};
+        System.out.println("Ans: " + firstOccurence(arr2, 2));
+    }
+}
---
+++ src/algorithms/f_searching/E_Sqrt.java
@@ -0,0 +1,30 @@
+package algorithms.f_searching;
+
+public class E_Sqrt {
+    public static int sqrt(int x) {
+        int y = 0;
+        while (y * y <= x) {
+            y++;
+        }
+        return y - 1;
+    }
+    public static int sqrt2(int x) {
+        int start  = 0, end = x;
+        int ans = -1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int val = mid * mid;
+            if (val == x) return mid;
+            else if (val > x) end = mid - 1;
+            else {
+                ans = mid;
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int x = 49;
+        System.out.println(sqrt(x));
+    }
+}
