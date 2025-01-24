86a8731bd8c8159c55dc00ce644d4afe180aa5f9
Satvik <sharmasatvik43@gmail.com>
2025-01-24 11:30:13 +0530

    Linear Search Coding implementation is done

---
+++ src/algorithms/f_searching/A_LinearSearch.java
@@ -0,0 +1,17 @@
+package algorithms.f_searching;
+// O(n) time complexity searching algorithm
+public class A_LinearSearch {
+    public static int linearSearch(int[] arr, int target) {
+        for (int i = 0 ; i < arr.length ; i++) {
+            if (arr[i] == target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {12, 34, 21, 55, 93, 45, 23, 67};
+        int target = 45;
+        System.out.println("Ans: " + linearSearch(arr, target));
+    }
+}
