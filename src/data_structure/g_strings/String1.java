1836911dd100ed78bc238bad478fa05206bb86a1
Satvik Sharma <sharmasatvik43@gmail.com>
2025-02-08 19:05:35 +0530

    String with its basic methods are done

---
+++ src/data_structure/g_strings/String1.java
@@ -0,0 +1,42 @@
+package data_structure.g_strings;
+import java.util.*;
+public class String1 {
+    public static void main(String[] args) {
+        String str = "Satvik";
+        String gtr = "ratvik";
+        System.out.println(str);
+
+        // charAt() function of string
+        System.out.println("Char At result: " + str.charAt(0));
+
+        // length() function of string
+        System.out.println("Length result: " + str.length());
+
+        // indexOf() function of string
+        // opposite of charAt(). indexOf() returns the index of given element
+        System.out.println("Index of result: " + str.indexOf("t"));
+
+        // compareTo() function of string
+        // returns 0  if both strings are same and returns +ve if first letter is of a string is lexicographically
+        // greater and returns -ve if that element is small.
+
+        System.out.println("Compare to result: " + str.compareTo(gtr));
+
+
+        // contains() function of string
+        System.out.println("Does vik present: " + str.contains("vik"));
+
+        // startsWith() function of string
+        System.out.println("Starts with Sat: " + str.startsWith("Sat"));
+
+        // endsWith() function of string
+        System.out.println("Ends with ik: " + str.endsWith("ik"));
+
+        // toLowerCase(), toUpperCase() functions of string
+        System.out.println(str.toLowerCase());
+        System.out.println(str.toUpperCase());
+
+        // concat(strName) function of string
+        System.out.println(str.concat(gtr));
+    }
+}
